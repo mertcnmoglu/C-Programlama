832617c9fe729be40fb94607b4716721de40053a
Mert İsa CANIMOĞLU <157281157+mertcnmoglu@users.noreply.github.com>
2024-04-14 12:21:41 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char *gun[7]={"pazartesi","sali","carsamba","persembe","cuma","cumartesi","pazar"};
+    int i;
+
+    do
+    {
+        printf("Lutfen 1 ve 7 arasinda bir deger giriniz\n");
+        scanf("%d",&i);
+    }
+    while(i<=0 || i>7);
+    printf("%d. Gun %s dir",i,gun[i-1]);
+
+
+
+
+
+    return 0;
+}
