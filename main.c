65fe3dcb585cd45c8a360211b1b3576971f129bc
Mert İsa CANIMOĞLU <157281157+mertcnmoglu@users.noreply.github.com>
2024-04-17 19:18:00 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int *ptr;
+
+    int n = 5;
+    int i;
+    printf("Girdigim sayinin degeri %d\n",n);
+
+    ptr=(int *)calloc(n,sizeof(int));
+
+    if(ptr == NULL)
+    {
+        printf("Yeterli hafiza ayrilamadi..!");
+    }
+    else
+    {
+        for(i=0;i<n;i++)
+        {
+            ptr[i]=i+1;
+        }
+        printf("Elemanlarim\n");
+        for(i=0;i<n;i++)
+        {
+            printf("%d, ",ptr[i]);
+        }
+    }
+
+    return 0;
+}
