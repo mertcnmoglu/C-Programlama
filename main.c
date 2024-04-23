5db4f34762a67ca25bce335cd4afc20faa7a5bb3
Mert İsa CANIMOĞLU <157281157+mertcnmoglu@users.noreply.github.com>
2024-04-23 23:19:04 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int *p;
+
+    //p=(int *)malloc(10*sizeof(int));
+    //p=(int *)malloc(40);   //ÜSTTEKİ İLE AYNI ANLAMI İFADE EDİYOR
+    p=(int *)calloc(10,sizeof(int));
+
+    printf("%d\n",p[0]);
+
+    p[0]=4;
+
+    printf("%d\n",p[0]);
+    free(p);
+
+
+    return 0;
+}
